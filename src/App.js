48a17627fd469b6c7630f84139b8dfed7bb662f8
Eduardo Guzman <edguzmanvel@gmail.com>
2024-03-12 02:29:29 -0500

    added components

--- src/App.js
+++ src/App.js
@@ -1,42 +1,64 @@
+import React, { Component } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import AppRoutes from './AppRoutes';
+import { Layout } from './components/Layout';
+import './custom.css';
 
-import './App.css';
-import { Component } from 'react';
+export default class App extends Component {
+  static displayName = App.name;
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      users: []
-    }
+  render() {
+    return (
+      <Layout>
+        <Routes>
+          {AppRoutes.map((route, index) => {
+            const { element, ...rest } = route;
+            return <Route key={index} {...rest} element={element} />;
+          })}
+        </Routes>
+      </Layout>
+    );
   }
+}
 
-  API_URL = "http://localhost:5026/";
+// import './App.css';
+// import { Component } from 'react';
 
-  componentDidMount(){
-    this.refreshUsers();
-  }
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       users: []
+//     }
+//   }
 
-  async refreshUsers(){
-    fetch(this.API_URL+"api/Values/GetUsers").then(response=>response.json())
-    .then(data=>{
-      this.setState({users:data});
-    })
-  }
-    render(){
-      const{users} = this.state;
-      return (
-      <div className="App">
-        <h2>New EOSR</h2>
+//   API_URL = "http://localhost:5026/";
 
-        {users.map(user=>
-          <p>
-            <b>
-              *{user.description}*
-            </b>
-          </p>
-        )}
-      </div>
-      );
-    }
-  }
-export default App;
+//   componentDidMount(){
+//     this.refreshUsers();
+//   }
+
+//   async refreshUsers(){
+//     fetch(this.API_URL+"api/Values/GetUsers").then(response=>response.json())
+//     .then(data=>{
+//       this.setState({users:data});
+//     })
+//   }
+//     render(){
+//       const{users} = this.state;
+//       return (
+//       <div className="App">
+//         <h2>New EOSR</h2>
+
+//         {users.map(user=>
+//           <p>
+//             <b>
+//               *{user.description}*
+//             </b>
+//           </p>
+//         )}
+//       </div>
+//       );
+//     }
+//   }
+// export default App;
