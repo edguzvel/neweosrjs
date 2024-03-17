a6db2bad060aaf9fbbc4a587df91b121fbfb57fe
Eduardo Guzman <edguzmanvel@gmail.com>
2024-03-16 22:50:36 -0500

    CODE FOR AUTHENTICATION

---
+++ src/Config.js
@@ -0,0 +1,8 @@
+export const config = {
+    appId: 'd76c8167-66ae-4153-81fc-5d7eb1b78620',
+    redirectUri: 'http://localhost:3000',
+    scopes: [
+        'user.read',
+    ],
+    authority: 'https://login.microsoftonline.com/ed416.onmicrosoft.com',
+};
