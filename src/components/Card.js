2c647277642cd2c57a5bbecd6cc1615c04a73759
Alberto Arciva <alberto.arciva@gmail.com>
2024-03-24 01:17:00 -0500

    Minor changes to landing page

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import './Login.css';
 import { SignInButton } from './SignInButton';
- 
+import landingPageImage from '../landingPageImage.png';
+
 const Card = (props) => {
   return (
-<div className="container">
-<div className="card">
-<h1>{props.children}</h1>
-</div>
-<div className="button-container">
-        {/* Assuming SignInButton renders a button element */}
-<SignInButton className="button" />
-</div>
-</div>
+    <div className="container">
+      <div className="logo">ShiftIO</div> {/* Add this line for the logo */}
+      <div className="card">
+        <img src={landingPageImage} alt="Landing Page" className="landing-image" />
+        <h1>{props.children}</h1>
+      </div>
+      <div className="button-container">
+        <SignInButton className="button" />
+      </div>
+    </div>
   );
 };
- 
+
 export default Card;
