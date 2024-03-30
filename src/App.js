8451ae3a3c325ab2cf4996e51390bd655abb9dcc
Eduardo Guzman <edguzmanvel@gmail.com>
2024-03-29 19:01:57 -0500

    API backend works

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AuthenticatedTemplate, UnauthenticatedTemplate } from '@azure/msal-react';
-import { Home } from './components/Home'; // Adjust imports as necessary
+import  AuthDisplay  from './components/AuthDisplay'; // Adjust imports as necessary
 
 import  Card  from './components/Card';
 
@@ -14,7 +14,7 @@ function App() {
           <Route path="/" element={
             <>
               <AuthenticatedTemplate>
-                <Home /> {/* Authenticated users will see this */}
+                <AuthDisplay /> {/* Authenticated users will see this */}
               </AuthenticatedTemplate>
               <UnauthenticatedTemplate>
   <Card /> {/* Unauthenticated users will see this */}
